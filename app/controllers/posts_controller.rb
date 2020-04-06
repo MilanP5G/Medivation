ecc6a37416868836c767e70cce6105e1fbc09aff
MilanP5G <milan_parmar@hotmail.co.uk>
2020-04-06 14:15:48 +0100

    create new erbs and edit user methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
 
+  get '/posts' do
+    @posts = Posts.all
+    erb :'/posts/index'
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if Helpers.logged_in?(session)
-      redirect '/posts'
+      redirect '/profile'
     end
       erb :'users/create_user'
   end
@@ -13,28 +13,34 @@ class UsersController < ApplicationController
         redirect '/signup'
       end
     end
-    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    user = User.create(username: params[:username], email: params[:email], password: params[:password])
     session[:user_id] = user.id
-    redirect '/posts'
+    redirect '/profile'
   end
 
   get '/login' do
     if Helpers.logged_in?(session)
-      redirect '/posts'
+      redirect '/profile'
     end
       erb :'/users/login'
   end
 
   post '/login' do
     user = User.find_by(username: params[:username]) || User.find_by(email: params[:email])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect '/posts'
+      redirect '/profile'
     end
-
     redirect '/login'
-    
   end
+
+  get '/profile' do
+    if !Helpers.logged_in?(session)
+      redirect '/login'
+    end
+     @user = Helpers.current_user(session)
+    erb :'/users/profile'
+ end
+
 
 end
