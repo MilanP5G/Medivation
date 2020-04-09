45240c3091b39042433e8dae3b66bb7fd0cdc0e4
MilanP5G <milan_parmar@hotmail.co.uk>
2020-04-09 18:05:37 +0100

    added if statement to home.erb to remove users that have no posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
 
-end 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+end
